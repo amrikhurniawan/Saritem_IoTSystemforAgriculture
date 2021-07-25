3a45357b03bbefde5bb40c99d746c68f49f2bdbc
amrikhurniawan <amrikhurniawanazga3@gmail.com>
2021-07-25 22:00:46 +0700

    Delete logout.js

--- logout.js
+++
@@ -1,9 +0,0 @@
-const logout = document.querySelector('#logout');
-logout.addEventListener('click', (e) => {
-	e.preventDefault();
-window.location.href = "index.html";
-auth.signOut().then(() => {
-		console.log('user signed out');
-
-});
-});
